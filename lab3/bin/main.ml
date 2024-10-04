ff5c5e30accff4fb9372eb43a79a6863a040b2ea
Henry <rtan035@uottawa.ca>
2024-10-04 14:25:04 -0400

    Changed implementation of record vehicle and added assign

--- lab3/bin/main.ml
+++ lab3/bin/main.ml
@@ -2,14 +2,15 @@
 
  type location = {
   name : string;
-  x : int;
-  y : int;
+  x : float;
+  y : float;
   priority : int;
 }
 
 type vehicle = {
   id : int;
   capacity : int;
+  locations : location list;
 }
 
 (*Auliary function for sorting the locations by priority*)
@@ -29,13 +30,41 @@ let ask_location_information location_num =
   Printf.printf "Priority: ";
   let priority = input_line stdin in
 
-  { name = name; x = (int_of_string x); y = (int_of_string y); priority = (int_of_string priority) }
+  { name = name; x = (float_of_string x); y = (float_of_string y); priority = (int_of_string priority) }
+
 
 let ask_vehicle_information vehicle_num = 
   Printf.printf "Enter details for location %d, please enter the following details:\nCapacity: " vehicle_num;
   let capacity = input_line stdin in 
-  { id = vehicle_num; capacity = (int_of_string capacity) }
+  { id = vehicle_num; capacity = (int_of_string capacity); locations = []}
   
+(*Function to calculate the distance between two points*)
+let calc_dist loc1 loc2 = 
+  sqrt((loc2.x -. loc1.x) *. (loc2.x -. loc1.x) +. (loc2.y -. loc1.y) *. (loc2.y -. loc1.y))
+
+
+(*Function for spliting a list of locations*)
+let rec split_loc n locations = 
+  (*If n is empty or the list is empty, return a tuple of empty list and original list of locations*)
+  if n <= 0 || List.length(locations) == 0 then
+    ([],locations)
+  else
+    let (head,rest) = split_loc (n-1) (List.tl locations) in (*Split the location list into head and rest*)
+    (List.hd locations::head,rest) (*concatenate *)
+
+(*Function to assign locations to vehicles based on the vehicle's capacity*)
+let assign locations vehicles = 
+  let sorted = sort_by_priority locations in (*Sort locations by priority*)
+  (*Auxiliary function for assigning vehicles*)
+  let rec assign_vehicle vehicles locations res = 
+    match vehicles with
+    | [] -> res (*Base case, all vehicles assigned, return resultant list*)
+    | head::tail -> 
+        let (assigned, remaining) = split_loc head.capacity locations in (*Split a vehicle's capacity of location into assigned, and the rest into remaining*)
+        let each = {head with locations = assigned} in (*Assign the assigned locations to the current vehicle*)
+        assign_vehicle tail remaining  (each :: res) in (*recurse on the reamining vehicles and locations*)
+  assign_vehicle vehicles sorted [] 
+    
 
 let read_locations num =
   let list_jobs = ref [] in
@@ -50,6 +79,16 @@ let read_vehicles num =
     list_vehicles := !list_vehicles @ [ask_vehicle_information i]
   done;
   !list_vehicles
+
+
+  (*function to display the optimized route for each vehicle*)
+  let print_locations_list locations =
+    List.iter (fun location ->
+      Printf.printf "name: %s name, priority: %d priority"
+        location.name location.priority
+    (*print out total distance here*)
+    
+    ) locations 
 
 (* Main method *)
 let () =
