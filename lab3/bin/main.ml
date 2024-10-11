c0f72babcfc434ae2050f1970215dafe7a7b0e6c
jordanxlau <jordanxlau@yahoo.ca>
2024-10-10 23:21:05 -0400

    replaced stdin with read_line

--- lab3/bin/main.ml
+++ lab3/bin/main.ml
@@ -20,16 +20,16 @@ let sort_by_priority locations =
 (* A function that prompts location details to be inputted via the command line *)
 let ask_location_information location_num = 
   Printf.printf "Enter details for location %d\nLocation name: " location_num;
-  let name = input_line stdin in 
+  let name = read_line () in 
 
   Printf.printf "X coordinate: ";
-  let x = input_line stdin in
+  let x = read_line () in
 
   Printf.printf "Y coordinate: ";
-  let y = input_line stdin in
+  let y = read_line () in
   
   Printf.printf "Priority: ";
-  let priority = input_line stdin in
+  let priority = read_line () in
 
   { name = name; x = (float_of_string x); y = (float_of_string y); priority = (int_of_string priority) }
 
@@ -37,7 +37,7 @@ let ask_location_information location_num =
 (* A function that prompts vehicle details to be inputted via the command line *)
 let ask_vehicle_information vehicle_num = 
   Printf.printf "Enter details for location %d, please enter the following details:\nCapacity: " vehicle_num;
-  let capacity = input_line stdin in 
+  let capacity = read_line () in 
   { id = vehicle_num; capacity = (int_of_string capacity); locations = []}
   
 
