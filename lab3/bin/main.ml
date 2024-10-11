2ecf98e55a4ed81d8a227b43a4dcc650b18f4bcf
zechen <zhou2067522448@gmail.com>
2024-10-11 01:13:31 -0400

    Implement function calc_total_dist() and finish main program. Known issue: unable to get the same distance as in the sample output.
    1. Update sort_by_priority() to sort the locations by priority in descending order
    2. Update `vehicle` structure to include location list
    3. Change function name from `print_locations_list` to `print_opt_route_distance`

--- lab3/bin/main.ml
+++ lab3/bin/main.ml
@@ -10,7 +10,9 @@
 type vehicle = {
   id : int;
   capacity : int;
+  locations : location list;
 }
+
 
 (* 
   This recursive function prompts the user to enter a floating-point number.
@@ -53,9 +55,11 @@ let rec get_non_negative_int_number prompt_message =
     print_endline "Invalid input. Please enter an integer (e.g. 4)\n";
     get_non_negative_int_number prompt_message    
 
-(*Auxiliary function for sorting the locations by priority*)
-(* let sort_by_priority locations = 
-  List.sort(fun l1 l2 -> (compare l1.priority l2.priority)) locations;; *)
+
+(*Auxiliary function for sorting the locations by priority in descending order *)
+let sort_by_priority locations = 
+  List.sort(fun l1 l2 -> (compare l2.priority l1.priority)) locations;;
+
 
 (* A function that prompts location details to be inputted via the command line *)
 let ask_location_information location_num = 
@@ -79,37 +83,50 @@ let ask_vehicle_information vehicle_num =
 
   print_endline "";
 
-  { id = vehicle_num; capacity = capacity;}
-  
-(* DEBUG *)
-(*Function to calculate the distance between two points*)
-(* let calc_dist loc1 loc2 = 
-  sqrt((loc2.x -. loc1.x) *. (loc2.x -. loc1.x) +. (loc2.y -. loc1.y) *. (loc2.y -. loc1.y)) *)
+  { id = vehicle_num; capacity = capacity; locations = []}
+
 
 (* DEBUG *)
+(*Function to calculate the distance between two points*)
+let calc_two_loc_dist loc1 loc2 = 
+  sqrt((loc2.x -. loc1.x) ** 2.0 +. (loc2.y -. loc1.y) ** 2.0)
+
+
+let rec calc_total_dist loc_list = 
+  match loc_list with
+  (* Base case: no distance if 0 of 1 location *)
+  | [] | [_] -> 0.0
+
+  | loc1::loc2::rest ->
+    let dist = calc_two_loc_dist loc1 loc2 in
+
+    (* Recurse with the rest of the list *)
+    dist +. calc_total_dist(loc2 :: rest)
+
+
 (*Function for spliting a list of locations*)
-(* let rec split_loc n locations = 
+let rec split_loc n locations = 
   (*If n is empty or the list is empty, return a tuple of empty list and original list of locations*)
   if n <= 0 || List.length(locations) == 0 then
     ([],locations)
   else
     let (head,rest) = split_loc (n-1) (List.tl locations) in (*Split the location list into head and rest*)
     (List.hd locations::head,rest) (*concatenate *)
-sad *)
+
+
 (*Function to assign locations to vehicles based on the vehicle's capacity*)
-(* let assign locations vehicles = 
+let assign locations vehicles = 
   let sorted = sort_by_priority locations in (*Sort locations by priority*)
   (*Auxiliary function for assigning vehicles*)
   let rec assign_vehicle vehicles locations res = 
     match vehicles with
-    | [] -> res (*Base case, all vehicles assigned, return resultant list*)
+    (* `List.rev` is used to reverse the accumulated list of vehicles (res), since we want the vehicle in the same order as before *)
+    | [] -> List.rev res (*Base case, all vehicles assigned, return resultant list*)
     | head::tail -> 
         let (assigned, remaining) = split_loc head.capacity locations in (*Split a vehicle's capacity of location into assigned, and the rest into remaining*)
         let each = {head with locations = assigned} in (*Assign the assigned locations to the current vehicle*)
         assign_vehicle tail remaining  (each :: res) in (*recurse on the reamining vehicles and locations*)
-  assign_vehicle vehicles sorted []  *)
-    
-
+  assign_vehicle vehicles sorted [] 
 
 
 (* A function that reads information for n locations by repeatedly calling ask_location_information n times 
@@ -121,6 +138,7 @@ let read_locations n =
   done;
   !list_locations
 
+
 (* A function that reads information for n vehicles by repeatedly calling ask_vehicle_information n times
   Returns a list of vehicle records *)
 let read_vehicles n =
@@ -131,25 +149,21 @@ let read_vehicles n =
   !list_vehicles
 
 
-  (*function to display the optimized route for each vehicle*)
-  let print_locations_list locations =
-    List.iter (fun location ->
-      Printf.printf "name: %s name, priority: %d priority\n"
-        location.name location.priority;
-      Printf.printf "x: %f x, y: %f y"
-      location.x location.y
-    (*print out total distance here*)
-    
-    ) locations 
+  (* function to display the optimized route and total distance for each vehicle *)
+  let print_opt_route_distance vehicles =
+    List.iter (fun v -> 
+      Printf.printf "Vehicle %d route: " v.id;
 
-      (*function to display the optimized route for each vehicle*)
-  let print_vehicle_list vehicles =
-    List.iter (fun vehicle ->
-      Printf.printf "id: %d, capacity: %d\n"
-      vehicle.id vehicle.capacity;
-    (*print out total distance here*)
-    
-    ) vehicles 
+      (* Inner loop: printe route *)
+      List.iter (fun loc -> Printf.printf "-> %s " loc.name) v.locations;
+      print_endline "";
+      
+      (* Calculate the total distance for the route *)
+      let total_distance = calc_total_dist(v.locations) in
+      Printf.printf "Total distance: %.2f km\n\n" total_distance;
+
+      ) vehicles
+
 
 (* Main method *)
 let () =
@@ -174,7 +188,7 @@ let () =
 
     else
       let vehicles = read_vehicles(num_vehicles) in
+      
+      let assigned_vehicles = assign locations vehicles in
 
-      (* DEBUG *)
-      print_locations_list(locations);
-      print_vehicle_list(vehicles);
+      print_opt_route_distance(assigned_vehicles);
